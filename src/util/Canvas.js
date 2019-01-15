4c9e5d88e78ccadd8ffb5599828dfdd77c9ccf00
Cazra <sllindberg21@gmail.com>
2019-01-15 15:21:03 -0500

    Implemented Canvas.clear to clear a drawing buffer with a specified color.

--- src/util/Canvas.js
+++ src/util/Canvas.js
@@ -1,9 +1,32 @@
 'use strict';
 
+import { Color } from '../materials';
+
 /**
  * Utilities concerning Canvas objects.
  */
 export class Canvas {
+
+  /**
+   * Clears the canvas bound to a GL object.
+   * @param {WebGLRenderingContext} gl
+   * @param {(KraGL.materials.Color|vec4)} [color]
+   *        The color used to clear the canvas.
+   *        If given as a vec4, it must be in normalized RGBA format.
+   *        By default, it will clear with transparent black [0,0,0,0].
+   * @param {GLEnum} [bufferBit]
+   *        The bit code for the buffer being cleared.
+   *        By default, this is the color buffer.
+   */
+  static clear(gl, color=[0,0,0,0], bufferBit=GL_COLOR_BUFFER_BIT) {
+    if(color instanceof Color)
+      color = color.rgba;
+    else if(color.length !== 4)
+      throw new KraGLError('Clear color must be in normalized RGBA format.');
+
+    gl.clearColor(...color);
+    gl.clear(bufferBit);
+  }
 
   /**
    * Resizes a WebGL canvas's pixel size to its display size.
