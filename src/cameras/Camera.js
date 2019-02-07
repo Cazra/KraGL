755333fb9ce4ba6dbd1301ed2f69ce086e992284
Cazra <sllindberg21@gmail.com>
2019-02-06 22:30:00 -0500

    Forgot to stage new files...

---
+++ src/cameras/Camera.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import { AbstractError } from '../AbstractError';
+
+/**
+ * Base class for all 2D and 3D cameras.
+ * @memberof KraGL.cameras
+ */
+export class Camera {
+
+  /**
+   * The projection transform for the camera.
+   * @type {mat4}
+   */
+  get projection() {
+    throw new AbstractError();
+  }
+
+  /**
+   * The view transform for the camera.
+   * @type {mat4}
+   */
+  get view() {
+    throw new AbstractError();
+  }
+
+  constructor(opts) {
+    _.noop(opts);
+  }
+}
---
+++ src/cameras/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/**
+ * This namespace contains classes for creating 2D and 3D camera transforms.
+ * @namespace cameras
+ * @memberof KraGL
+ */
+export * from './Camera';
